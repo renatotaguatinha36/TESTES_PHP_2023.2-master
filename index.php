e2731c1d4901e74e771e6ed801c57dc13f41b734
renatoguara2020 <renatoguara2020@gmail.com>
2023-10-17 08:57:40 -0300

    ok

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $result = $conn->query('SELECT * FROM users ORDER BY id ASC');
 <body>
 
     <a href="adicionar.html">Cadastrar Usuário</a>
+    &nbsp;&nbsp;&nbsp;
     <table class="table table-dark table-striped">
 
         <thead>
@@ -43,7 +44,7 @@ $result = $conn->query('SELECT * FROM users ORDER BY id ASC');
             echo '<td>' . $row['idade'] . '</td>';
             echo '<td>' . $row['email'] . '</td>';
             echo '<td>' . $row['cidade'] . '</td>';
-            echo '<td>' . date('d/m/Y'), strtotime($row['dataAtual']) . '</td>';
+            echo '<td>' . date('d/m/Y H:i:s'), strtotime($row['dataAtual']) . '</td>';
             echo "<td><a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
         }
         echo '</tr>';
