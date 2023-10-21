0875f2a83b1f9ecd5cf85e4ba7597a3f693caa54
renatoguara2020 <renatoguara2020@gmail.com>
2023-10-21 11:02:16 -0300

    ok

--- array_reverse.php
+++ array_reverse.php
@@ -1,6 +1,10 @@
 <?php
-$arr = array("laptop", "mobile","tablet");
+$arr = array("laptop", "mobile","tablet","galaxy A300rtr");
+$arr1 = ["laptop", "mobile","tablet","galaxy A300rtr"];
 $reverse = array_reverse($arr);
+$reverse1 = array_reverse($arr1);
 print_r($arr);
+print_r($arr1);
 print_r($reverse);
+print_r($reverse1);
 ?>
