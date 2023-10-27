a59d744e48995d65911b8b297aa49bb11f0f1ef1
renatoguara2020 <renatoguara2020@gmail.com>
2023-10-27 07:18:14 -0300

    ok

---
+++ listas.py
@@ -0,0 +1,9 @@
+nomes = ['RENATO', 'CREUZA', 'FERNANDO', 'NATHAN','RUBENS','RUBIANA','WALTER','ARTHUR']
+
+
+# print(nomes)
+
+
+# print(nomes[::-1])
+# nomes.reverse()
+print(nomes[0:6])
---
+++ testes.py
@@ -0,0 +1,21 @@
+
+
+nome = "Renato Alves Soares"
+idade = 52
+valor = 12.909090
+PI = 3.14212324
+
+numero1 = 21
+numero2 = 2
+resultado = numero1 % numero2
+potencia = 2 ** 10
+
+nomes = ['renato', 'sandra','rubens','creuza']
+
+print(type(nome))
+print(type(idade))
+print(type(valor))
+print(type(nomes))
+print(type(PI))
+print(resultado)
+print(potencia)
