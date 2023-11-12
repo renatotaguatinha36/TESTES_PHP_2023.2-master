a4e5503a409e91ee3e1d08c03c6666415ab74275
renatoguara2020 <renatoguara2020@gmail.com>
2023-11-12 09:19:03 -0300

    ok

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@ $conn = new PDO("mysql:host=localhost;dbname=test1", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-// echo "<h3>Connected Successfully with Database: $database</h3>";
+echo "<h3>Connected Successfully with Database: $database</h3>";
 
 }catch(PDOException $e){
 
