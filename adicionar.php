8fbe8f75977b0223d49cc57a90d550d1618581ec
renatoguara2020 <renatoguara2020@gmail.com>
2023-12-11 06:05:10 -0300

    ok

--- adicionar.php
+++ adicionar.php
@@ -1,5 +1,7 @@
+
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet/>
-<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php
 
@@ -45,3 +47,4 @@ header("Location: index.php");
 
 
 }
+
